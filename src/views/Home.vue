b5ecf1d3906e732678ef9e8a51f76abf46be9711
trickypr <trickypr@users.noreply.github.com>
2019-03-07 18:17:26 +1100

    Add vue

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="home">
+  <header>
+
+  </header>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default
+{
+  name: 'home',
+  components:
+  {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    }
+</style>
