496263bbb326b26c0c1d6c18ef11b563565bc90e
trickypr <trickypr@users.noreply.github.com>
2019-04-10 16:26:06 +1000

    Add logo and oterstuff

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import Nav from './components/Nav.vue'
 
 export default
 {
-  compoents:
+  components:
   {
     Nav
   }
@@ -18,21 +18,15 @@ export default
 </script>
 
 <style lang="scss">
+body {
+    margin: 0;
+}
+
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-}
-#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
+
+    margin: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
 <div class="home">
   <header>
-
+    <div class="text">
+      <h1>Glarce</h1>
+      <p>Web AR, simplified</p>
+    </div>
   </header>
+
+  <div class="features">
+    <div>
+
+    </div>
+  </div>
 </div>
 </template>
 
@@ -20,6 +29,37 @@ export default
 </script>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Quicksand');
+
 header {
+    height: 102vh;
+    background: rgb(42,250,223);
+    background: linear-gradient(210deg, rgba(42,250,223,1) 0%, rgba(76,131,255,1) 100%);
+
+    color: #eee;
+    font-family: 'Quicksand', sans-serif;
+
+    border-bottom-left-radius: 50% 2vh;
+    border-bottom-right-radius: 50% 2vh;
+
+    .text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        h1 {
+            font-size: 96pt;
+            margin: 0;
+        }
+        p {
+            font-size: 20pt;
+            margin: 0;
+        }
     }
+}
+
+.features {
+  height: 100vh;
+}
 </style>
