73c41aa63ad9932a8e6b5990432704cab16bab00
trickypr <trickypr@users.noreply.github.com>
2019-04-24 16:29:27 +1000

    Add theme standard and favicon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: '/Website/'
+  publicPath: '/Website/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
 }
